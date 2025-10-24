3df28ec2ab1e07e4e1cc7ccbcd472e7602b553df
junietan <junietan@gmail.com>
2025-10-24 17:37:03 +0800

    a

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 animal_shelter = ["dog", "cat", "rabbit", "hamster", "parrot"]
 animal = st.text_input("Enter your favorite animal:")
 if st.button("Check Animal"):
-    if animal.lower in animal_shelter:
+    if animal.lower() in animal_shelter:
         st.success(f"{animal} is available for adoption!")
     else:
         st.error(f"Sorry, {animal} is not available for adoption.")
